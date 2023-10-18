166983c758950bede404108c792acf5db4ebe31e
Marcos7845 <marcos7845@gmail.com>
2023-10-18 13:59:28 -0300

    ufa!

---
+++ ModuloD/ex008.js
@@ -0,0 +1,7 @@
+var vel = 70
+ console.log("A sua velocidade é de " + vel + "km/h")
+if(vel>60){
+    console.log("Você está multado por excesso de volecidade")
+}
+console.log("dirija com segurança"
+)
---
+++ ModuloD/ex009.js
@@ -0,0 +1,14 @@
+//verificar Nacionalidade
+
+//var pais ="França"
+//console.log("Você vive em " + pais)
+//if(pais!="Brasil"){
+
+   // console.log("Você é estrangeiro")
+
+//}//else{//
+    //console.log("Você é brasileiro")
+//}
+var estrangeiro = window.document.getElementById('estg')
+       
+        
